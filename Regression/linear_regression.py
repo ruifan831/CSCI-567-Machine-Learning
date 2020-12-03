1c772be3bc2357cc01b95114e4cb14827453fd06
RUIFANXU <ruifan831@gmail.com>
2020-12-02 18:51:33 -0800

    first commit

---
+++ Regression/linear_regression.py
@@ -0,0 +1,111 @@
+import numpy as np
+import pandas as pd
+
+############################################################################
+# DO NOT MODIFY CODES ABOVE 
+# DO NOT CHANGE THE INPUT AND OUTPUT FORMAT
+############################################################################
+
+###### Part 1.1 ######
+def mean_square_error(w, X, y):
+    """
+    Compute the mean square error of a model parameter w on a test set X and y.
+    Inputs:
+    - X: A numpy array of shape (num_samples, D) containing test features
+    - y: A numpy array of shape (num_samples, ) containing test labels
+    - w: a numpy array of shape (D, )
+    Returns:
+    - err: the mean square error
+    """
+    #####################################################
+    # TODO 1: Fill in your code here                    #
+    #####################################################
+    predict = X.dot(w)
+    err = np.sum(np.power(predict-y,2))/X.shape[0]
+    return err
+
+###### Part 1.2 ######
+def linear_regression_noreg(X, y):
+  """
+  Compute the weight parameter given X and y.
+  Inputs:
+  - X: A numpy array of shape (num_samples, D) containing features
+  - y: A numpy array of shape (num_samples, ) containing labels
+  Returns:
+  - w: a numpy array of shape (D, )
+  """
+  #####################################################
+  #	TODO 2: Fill in your code here                    #
+  #####################################################		
+  w = np.linalg.inv(np.dot(X.T,X)).dot(X.T).dot(y)
+  return w
+
+
+###### Part 1.3 ######
+def regularized_linear_regression(X, y, lambd):
+    """
+    Compute the weight parameter given X, y and lambda.
+    Inputs:
+    - X: A numpy array of shape (num_samples, D) containing features
+    - y: A numpy array of shape (num_samples, ) containing labels
+    - lambd: a float number specifying the regularization parameter
+    Returns:
+    - w: a numpy array of shape (D, )
+    """
+  #####################################################
+  # TODO 4: Fill in your code here                    #
+  #####################################################		
+    w = np.linalg.inv(np.dot(X.T,X)+lambd*np.identity(X.shape[1])).dot(X.T).dot(y)
+    return w
+
+###### Part 1.4 ######
+def tune_lambda(Xtrain, ytrain, Xval, yval):
+    """
+    Find the best lambda value.
+    Inputs:
+    - Xtrain: A numpy array of shape (num_training_samples, D) containing training features
+    - ytrain: A numpy array of shape (num_training_samples, ) containing training labels
+    - Xval: A numpy array of shape (num_val_samples, D) containing validation features
+    - yval: A numpy array of shape (num_val_samples, ) containing validation labels
+    Returns:
+    - bestlambda: the best lambda you find among 2^{-14}, 2^{-13}, ..., 2^{-1}, 1.
+    """
+    #####################################################
+    # TODO 5: Fill in your code here                    #
+    #####################################################		
+    bestlambda = None
+    loss = float("inf")
+    for i in range(-14,1):
+      temp_w = regularized_linear_regression(Xtrain,ytrain,2**i)
+      temp_loss = mean_square_error(temp_w,Xval,yval)
+      if temp_loss<loss:
+        loss = temp_loss
+        bestlambda = 2**i
+    return bestlambda
+    
+
+###### Part 1.6 ######
+def mapping_data(X, p):
+    """
+    Augment the data to [X, X^2, ..., X^p]
+    Inputs:
+    - X: A numpy array of shape (num_training_samples, D) containing training features
+    - p: An integer that indicates the degree of the polynomial regression
+    Returns:
+    - X: The augmented dataset. You might find np.insert useful.
+    """
+    #####################################################
+    # TODO 6: Fill in your code here                    #
+    #####################################################	
+    temp=[]
+    for i in range(2,p+1):
+      temp.append(np.power(X,i))
+    for i in temp:
+      X=np.insert(X,[X.shape[1]],i,axis=1)
+    return X
+
+"""
+NO MODIFICATIONS below this line.
+You should only write your code in the above functions.
+"""
+
